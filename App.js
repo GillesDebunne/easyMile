128f5897503f98b866599eeb8876cc807a6cf9c8
Gilles Debunne <gilles.debunne@gmail.com>
2017-04-21 11:51:06 +0200

    Read markers and lines from site.json file

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { MapView } from 'expo';
 
-const polyline = [
-  { latitude: 37.79, longitude: -122.44 },
-  { latitude: 37.80, longitude: -122.44 },
-  { latitude: 37.80, longitude: -122.45 },
-  { latitude: 37.79, longitude: -122.45 },
-]
+import site from './site.json';
 
 export default class App extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
-      markers: [
-        {
-          coordinate: { latitude: 37.79, longitude: -122.44 },
-          title: 'TEST',
-          description: 'A description...',
-        },
-      ]
+      markers: site.Lines.reduce((acc, lines) => {
+        lines.points.filter(point => point.station).forEach(point => {
+          acc.push({
+            coordinate: { latitude: point.lat, longitude: point.lng },
+            title: point.station,
+          })
+        })
+        return acc
+      }, []),
+      polylines: site.Lines.map(lines => (
+        lines.points.map(point => ({
+          latitude: point.lat,
+          longitude: point.lng,
+        }))
+      )),
+    }
+  }
+
+  initialRegion() {
+    const MARGIN = 1.2
+    return {
+      latitude: site.Map.origin.lat,
+      longitude: site.Map.origin.lng,
+      latitudeDelta: MARGIN * Math.abs(site.bounds.max.lat - site.bounds.min.lat),
+      longitudeDelta: MARGIN * Math.abs(site.bounds.max.lng - site.bounds.min.lng),
     }
   }
 
@@ -26,18 +39,15 @@ export default class App extends React.Component {
     return (
       <MapView
         style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={this.initialRegion()}
       >
+        {this.state.polylines.map((polyline, index) => (
+          <MapView.Polyline key={index} coordinates={polyline} strokeWidth={5}  strokeColor="#1396A5" />
+        ))}
+
         {this.state.markers.map((marker, index) => (
           <MapView.Marker key={index} {...marker} />
         ))}
-
-        <MapView.Polyline coordinates={polyline} />
       </MapView>
     );
   }
